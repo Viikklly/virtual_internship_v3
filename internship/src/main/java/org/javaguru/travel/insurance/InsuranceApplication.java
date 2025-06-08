3688ff973ae4ad513483e8efe07cfa2e38702ed5
Viikklly <94692215+Viikklly@users.noreply.github.com>
2025-06-08 10:31:17 +0000

    Добавили hello world!

--- internship/src/main/java/org/javaguru/travel/insurance/InsuranceApplication.java
+++ internship/src/main/java/org/javaguru/travel/insurance/InsuranceApplication.java
@@ -8,6 +8,7 @@ public class InsuranceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InsuranceApplication.class, args);
+        System.out.println("Hello, world!");
     }
 
 }
